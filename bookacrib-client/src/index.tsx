f8f2bbc2c149df1df20414064fdab07d91e8f13c
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-05 13:32:53 +0200

    Added route to Login page

--- bookacrib-client/src/index.tsx
+++ bookacrib-client/src/index.tsx
@@ -8,13 +8,18 @@ import {
   RouterProvider,
   Route,
 } from "react-router-dom";
-import { Home } from './routes/home';
+import { Home } from './routes/Home';
+import { Login } from './routes/Login';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home/>,
   },
+  {
+    path: "login",
+    element: <Login/>
+  }
 ]);
 
 const root = ReactDOM.createRoot(
