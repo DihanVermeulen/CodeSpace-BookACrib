a957cb3ec6481aca147ef092d7537f15c7324096
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-05 14:26:17 +0200

    Added logo to header

--- bookacrib-client/src/routes/Home.tsx
+++ bookacrib-client/src/routes/Home.tsx
@@ -1,4 +1,6 @@
-import { useNavigate } from "react-router-dom" ;
+import { useNavigate } from "react-router-dom";
+import '../styles.css';
+import logo from '../assets/images/logo.png';
 
 export const Home = () => {
 
@@ -6,10 +8,18 @@ export const Home = () => {
 
     return (
         <div id="home-page">
-            <header className="home-page--main-header"></header>
-            <nav className="home-page--vertical-navigation"></nav>
+            <header className="home-page--main-header">
+                <picture>
+                    <img className="home-page--main-header--logo" src={logo}></img>
+                </picture>
+            </header>
+            <nav className="home-page--vertical-navigation">
+                Logout
+            </nav>
             <main className="home-page--main-section"></main>
-            <footer className="home-page--footer"></footer>
+            <footer className="home-page--footer">
+                All Rights Reserved
+            </footer>
         </div>
     )
 }
