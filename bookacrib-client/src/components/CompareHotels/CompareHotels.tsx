c52b084e23eeb1fa5ff0f477619b5729a3c43096
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-07 15:12:03 +0200

    Checks query parameters and gets hotel id

--- bookacrib-client/src/components/CompareHotels/CompareHotels.tsx
+++ bookacrib-client/src/components/CompareHotels/CompareHotels.tsx
@@ -1,7 +1,32 @@
-export const CompareHotels = () => {
-    return (
-        <section className="home-page--main-section--card">
-            Compare Hotels
-        </section>
-    )
+import { useSearchParams } from "react-router-dom";
+import { JSXElementConstructor, ReactElement, ReactFragment, ReactPortal, useEffect, useState } from "react";
+import { api } from "../../axios";
+
+interface IHotel {
+    hotel_name: string,
+    rate: number,
+    rating: number,
+    image: string
+}
+
+export const CompareHotels: React.FC = (): any => {
+    const [searchParams] = useSearchParams();
+    const [hotel, setHotel] = useState<any>([]);
+
+    useEffect(() => {
+        let hotelIndex: any = searchParams.get('hotel');
+        api.get('/hotels')
+            .then((response): any => {
+                let selectedHotel = response.data[hotelIndex];
+                setHotel(selectedHotel);
+            });
+    }, [])
+
+    if (hotel) {
+        return (
+            <section className="home-page--main-section--card">
+                <div>{hotel.hotel_name}</div>
+            </section>
+        )
+    }
 }
