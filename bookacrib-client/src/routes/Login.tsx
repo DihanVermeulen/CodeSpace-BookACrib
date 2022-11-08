5fca45063116418c29fa49ff1e1e187f39a2d563
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-08 09:40:32 +0200

    Added navigation rerouting

--- bookacrib-client/src/routes/Login.tsx
+++ bookacrib-client/src/routes/Login.tsx
@@ -1,7 +1,18 @@
+import { Outlet, useNavigate } from "react-router-dom";
+import { useEffect } from 'react';
+
 export const Login = () => {
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      navigate('login');
+    }, [])
+    
+
     return (
         <div id="login-page">
-            Login
+            <Outlet />
         </div>
     )
 }
