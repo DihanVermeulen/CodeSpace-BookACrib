a3681f28bff97ca24cc68d0bbcf9af5d0e0ddec5
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-08 12:40:27 +0200

    Created form

--- bookacrib-client/src/components/LoginCard/LoginCard.tsx
+++ bookacrib-client/src/components/LoginCard/LoginCard.tsx
@@ -4,6 +4,21 @@ export const LoginCard = () => {
     return (
         <section className="login-page--card">
             <h1>Sign in</h1>
+            <form method='get'>
+                <div className='group'>
+                    <input className="input" type="text" required/>
+                    <span className="highlight"></span>
+                    <span className="bar"></span>
+                    <label className="label">Email</label>
+                </div>
+                <div className='group'>
+                    <input className='input' type="text" required/>
+                    <span className="highlight"></span>
+                    <span className="bar"></span>
+                    <label className='label'>Password</label>
+                </div>
+                <span className='bar'></span>
+            </form>
         </section>
     )
 }
