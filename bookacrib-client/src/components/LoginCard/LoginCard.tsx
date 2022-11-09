5c58046cb204f336401bd01f7d504a03f51575f1
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-09 11:34:07 +0200

    Updated

--- bookacrib-client/src/components/LoginCard/LoginCard.tsx
+++ bookacrib-client/src/components/LoginCard/LoginCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './LoginCard.css';
+import '../../utils/utils.css';
 
 interface props {
     setState: React.Dispatch<React.SetStateAction<any>>
@@ -11,20 +12,21 @@ export const LoginCard: React.FC<props> = ({setState}: props) => {
             <h1>Sign in</h1>
             <form method='get'>
                 <div className='group'>
-                    <input className="input" type="text" required/>
+                    <input id='email' autoFocus className="input" type="text" required/>
                     <span className="highlight"></span>
                     <span className="bar"></span>
                     <label className="label">Email</label>
                 </div>
                 <div className='group'>
-                    <input className='input' type="text" required/>
+                    <input id='password' className='input' type="text" required/>
                     <span className="highlight"></span>
                     <span className="bar"></span>
                     <label className='label'>Password</label>
                 </div>
+                <input type='submit' />
             </form>
             <p>Need an account?</p>
-            <div onClick={() => {setState(false)}}>Sign up</div>
+            <a className='link' onClick={() => {setState(false)}}>Sign up</a>
         </section>
     )
 }
