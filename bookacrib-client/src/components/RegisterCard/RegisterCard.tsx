4951407acfa6cf09cbff43fd5497037048004b5b
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-09 11:32:08 +0200

    Added template to form

--- bookacrib-client/src/components/RegisterCard/RegisterCard.tsx
+++ bookacrib-client/src/components/RegisterCard/RegisterCard.tsx
@@ -1,11 +1,28 @@
+import '../../utils/utils.css';
+
 interface props {
     setState: React.Dispatch<React.SetStateAction<any>>
 }
 
-export const RegisterCard: React.FC<props> = ({setState}) => {
+export const RegisterCard: React.FC<props> = ({ setState }) => {
     return (
         <section className="login-page--card">
-            <div onClick={() => {setState(true)}}>Go back</div>
+            <a className='link' onClick={() => { setState(true) }}>Back to login</a> {/* Sets parent state to show login card to true */}
+            <h1>Create Account</h1>
+            <form method='post'>
+                <div className='group'>
+                    <input id='email' className="input" type="text" required />
+                    <span className="highlight"></span>
+                    <span className="bar"></span>
+                    <label className="label">Email</label>
+                </div>
+                <div className='group'>
+                    <input id='password' className="input" type="text" required />
+                    <span className="highlight"></span>
+                    <span className="bar"></span>
+                    <label className="label">password</label>
+                </div>
+            </form>
         </section>
     )
 }
