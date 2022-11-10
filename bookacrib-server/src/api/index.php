3524dc00c258e0a0156e413fbd74b48f6587c271
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-10 19:02:56 +0200

    Updated post request and added extra headers
    Post request can now receive request values as json and add those values to database. Updated headers to allow all headers and all methods

--- bookacrib-server/src/api/index.php
+++ bookacrib-server/src/api/index.php
@@ -3,15 +3,15 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+include '../model/user.php';
+
 require '../vendor/autoload.php';
 
-$app = new \Slim\App([
-    "settings"  => [
-        "determineRouteBeforeAppMiddleware" => true,
-    ]
-]);
+$app = new \Slim\App();
 
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Methods: GET,PUT,POST,DELETE,PATCH,OPTIONS');
 
 $app->get('/hotels', function () {
     require_once('../dbconn/dbconn.php');
@@ -25,4 +25,54 @@ $app->get('/hotels', function () {
 
     echo json_encode($response);
 });
+
+$app->post('/users', function (Request $request, Response $response) {
+    require_once('../dbconn/dbconn.php');
+
+    // $user = new User($request->getParsedBody()['userName'], $request->getParsedBody()['userEmail'], $request->getParsedBody()['userPassword'], $request->getParsedBody()['userRole']);
+    $data = $request->getParsedBody();
+
+    // $test_user_name = $request->params;
+
+    $requestData = $request->getParsedBody();
+
+    $user = new User($requestData['userName'], $requestData['userEmail'], $requestData['userPassword'], $requestData['userRole']);
+    $userObject = $user->createUser();
+
+    // $html = var_export($userObject, true);
+    // $response->getBody()->write($html);
+
+    $user_id = $userObject['user_id'];
+    $user_name = $userObject['user_name'];
+    $user_email = $userObject['user_email'];
+    $user_password = $userObject['user_password'];
+
+    $query = "INSERT INTO users (user_id, user_name, user_email, user_password) VALUES (?,?,?,?)";
+    // $query = "INSERT INTO users (user_id, user_name, user_email, user_password) VALUES ($user_id,$user_name, $user_email,$user_password)";
+    // $response->getBody()->write(json_encode($stmt));
+    try {
+        $stmt = $db_connection->prepare($query);
+
+        $stmt->bind_param('ssss', $user_id, $user_name, $user_email, $user_password);
+
+        $result = $stmt->execute();
+
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(200);
+    } catch (PDOException $e) {
+        $error = array(
+            "message" => $e->getMessage()
+        );
+        $response->getBody()->write(json_encode($error));
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(500);
+    }
+});
+
+$app->get('/users', function () {
+});
+
+
 $app->run();
