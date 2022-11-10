2c403c78c8e88bf0a12c063d54e47e32f3d5b258
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-10 11:37:27 +0200

    Updated
    Fixed problem where selected hotel didn't change

--- bookacrib-client/src/components/CompareHotels/CompareHotels.tsx
+++ bookacrib-client/src/components/CompareHotels/CompareHotels.tsx
@@ -1,6 +1,7 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
-import { JSXElementConstructor, ReactElement, ReactFragment, ReactPortal, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../axios";
+import { fetchSearchParams, scrollPageToTop } from '../../utils/utils';
 
 interface IHotel {
     hotel_name: string,
@@ -18,50 +19,51 @@ export const CompareHotels: React.FC = (): any => {
 
     // Sets hotel state to hotel specified in search params
     useEffect(() => {
-        let hotelIndex: any = searchParams.get('hotel'); // Gets search paramaters
+        let hotelIndex: any = fetchSearchParams(searchParams, "hotel"); // Gets search paramaters
         api.get('/hotels')
             .then((response): any => {
                 let selectedHotel = response.data[hotelIndex];
                 setAllHotels(response.data);
                 setHotel(selectedHotel);
             });
-    }, [])
+    }, [hotel])
 
-    if (hotel) {
-        return (
-            <section className="main-section--card">
-                <p>Selected hotel: {hotel.hotel_name}</p>
-                <br />
-                <form>
-                    <div className="main-section--card-form-group">
-                        <label>Arrival Date</label>
-                        <input name="arrivalDate" type="date" />
-                    </div>
-                    <div className="main-section--card-form-group">
-                        <label>Departure Date</label>
-                        <input name="departureDate" type="date" />
-                    </div>
-                    <input type="submit" value="Compare" />
-                </form>
-                <hr />
-                <section className="main-section--card--hotel-section">
-                    {allHotels.map((hotel, key) => {
-                        return (
-                            <article key={key} className='main-section--card--hotel-card'>
-                                <img src={hotel.image} alt="hotel"></img>
-                                <div>{hotel.hotel_name}</div>
-                                <div>hotel rating:{hotel.hotel_rating}</div>
-                                <button onClick={() => {
-                                    navigate({
-                                        pathname: 'compare-hotels',
-                                        search: `?hotel=${key}`,
-                                    });
-                                }}>Book Now</button>
-                            </article>
-                        )
-                    })}
-                </section>
+    return (
+        <section className="main-section--card">
+            <p>Selected hotel: {hotel?.hotel_name}</p>
+            <br />
+            <form>
+                <div className="main-section--card-form-group">
+                    <label>Arrival Date</label>
+                    <input name="arrivalDate" type="date" />
+                </div>
+                <div className="main-section--card-form-group">
+                    <label>Departure Date</label>
+                    <input name="departureDate" type="date" />
+                </div>
+                <input type="submit" value="Compare" />
+            </form>
+            <hr />
+            <section className="main-section--card--hotel-section">
+                {allHotels.map((hotel, key) => {
+                    return (
+                        <article key={key} className='main-section--card--hotel-card'>
+                            <img src={hotel.image} alt="hotel"></img>
+                            <div>{hotel.hotel_name}</div>
+                            <div>hotel rating:{hotel.hotel_rating}</div>
+                            <button onClick={() => {
+                                navigate({
+                                    pathname: '/compare-hotels',
+                                    search: `?hotel=${key}`,
+                                });
+                                setHotel(allHotels[fetchSearchParams('hotel')]);
+                                scrollPageToTop();
+                                console.log(hotel);
+                            }}>Book Now</button>
+                        </article>
+                    )
+                })}
             </section>
-        )
-    }
+        </section>
+    )
 }
