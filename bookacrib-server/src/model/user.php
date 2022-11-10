d4354ad5e68cd1d5003689481f5fd337b66da629
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-10 19:04:53 +0200

    Updated class
    Class can now generate a random id, as well as create an encrypted password

--- bookacrib-server/src/model/user.php
+++ bookacrib-server/src/model/user.php
@@ -1,26 +1,61 @@
 <?php
 
-include __DIR__ . '../interface/userInterface.php';
-
-class User implements UserInterface
+class User
 {
     private $user;
     private $email;
     private $password;
+    private $role;
 
-    public function __construct($userInput, $emailInput, $passwordInput)
+    public function __construct($user, $email, $password, $role)
     {
-        $this->user = $userInput;
-        $this->email = $emailInput;
-        $this->password = $passwordInput;
+        $this->user = $user;
+        $this->email = $email;
+        $this->password = $password;
+        $this->role = $role;
     }
+
+    private function generateId()
+    {
+
+        $randomNumber = rand(10000, 90000);
+        $id = md5($randomNumber);
+
+        switch ($this->role) {
+
+            case 'admin':
+                $adminCode = "A#";
+                return $adminCode . $id;
+                break;
+
+            case 'customer':
+                $customerCode = "C#";
+                return $customerCode . $id;
+                break;
+
+            default:
+                return "error in creating user id";
+                break;
+        }
+    }
+
+    public function createUser()
+    {
+        return array(
+            "user_id" => $this->generateId(),
+            "user_name" => $this->user,
+            "user_password" => password_hash($this->password, PASSWORD_BCRYPT),
+            "user_email" => $this->email,
+        );
+    }
+
 
     /**
      * Set the value of user
      *
      * @return  self
      */
-    public function setUser($user)          
+    public function setUser($user)
     {
         $this->user = $user;
 
@@ -37,7 +72,7 @@ class User implements UserInterface
 
     /**
      * Get the value of email
-     */ 
+     */
     public function getEmail()
     {
         return $this->email;
@@ -47,7 +82,7 @@ class User implements UserInterface
      * Set the value of email
      *
      * @return  self
-     */ 
+     */
     public function setEmail($email)
     {
         $this->email = $email;
@@ -57,7 +92,7 @@ class User implements UserInterface
 
     /**
      * Get the value of password
-     */ 
+     */
     public function getPassword()
     {
         return $this->password;
@@ -67,7 +102,7 @@ class User implements UserInterface
      * Set the value of password
      *
      * @return  self
-     */ 
+     */
     public function setPassword($password)
     {
         $this->password = $password;
