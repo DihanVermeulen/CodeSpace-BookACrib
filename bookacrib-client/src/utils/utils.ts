33ad3845ec17c90966c688918df4bfa4a0a72e0b
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-11 14:40:15 +0200

    Created function to login

--- bookacrib-client/src/utils/utils.ts
+++ bookacrib-client/src/utils/utils.ts
@@ -1,3 +1,5 @@
+import { api } from "../api/axios";
+
 /**
  * Fetches all parameters in search and return as an array
  * @param args All parameters that will be fetched
@@ -15,5 +17,20 @@ export const fetchSearchParams = (searchParams: any, ...args: string[]): any => 
  * Scrolls page to top
  */
 export const scrollPageToTop = () => {
-    document.body.scrollTop = document.documentElement.scrollTop = 0; 
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+}
+
+export const login = () => {
+    api.post('/login', JSON.stringify({
+        user_update: 1,
+        user_id: "C#32b31047bf68aefa8bb491d46a432e8c"
+    }),
+        {
+            headers: {
+                'Authorization': 'Basic xxxxxxxxxxxxxxxxxxx',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((response) => console.log(response))
+        .catch((err) => console.log(err));
 }
