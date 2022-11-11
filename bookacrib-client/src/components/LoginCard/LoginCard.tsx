e2b5d7a811aed6dc244a1c0614be9a5dc7ddf3dd
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-11 14:41:31 +0200

    Added onsubmit function to login

--- bookacrib-client/src/components/LoginCard/LoginCard.tsx
+++ bookacrib-client/src/components/LoginCard/LoginCard.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import './LoginCard.css';
 import '../../utils/utils.css';
+import { login } from '../../utils/utils';
 
 interface props {
     setState: React.Dispatch<React.SetStateAction<any>>
+}
+
+const handleSubmit = (event:any) => {
+    event.preventDefault();
+    console.log('submitted');
+    login();
 }
 
 export const LoginCard: React.FC<props> = ({setState}: props) => {
     return (
         <section className="login-page--card">
             <h1>Sign in</h1>
-            <form method='get'>
+            <form id='loginForm' method='post' onSubmit={handleSubmit}>
                 <div className='group'>
                     <input id='email' autoFocus className="input" type="text" required/>
                     <span className="highlight"></span>
@@ -23,7 +30,7 @@ export const LoginCard: React.FC<props> = ({setState}: props) => {
                     <span className="bar"></span>
                     <label className='label'>Password</label>
                 </div>
-                <input type='submit' />
+                <input type='submit' value='Login' />
             </form>
             <p>Need an account?</p>
             <a className='link' onClick={() => {setState(false)}}>Sign up</a>
