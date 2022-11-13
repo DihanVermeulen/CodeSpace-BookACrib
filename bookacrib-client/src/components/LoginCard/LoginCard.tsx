bd646808aae576c5b07c78d9eb19998fed75b8bc
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-13 17:02:07 +0200

    Updated

--- bookacrib-client/src/components/LoginCard/LoginCard.tsx
+++ bookacrib-client/src/components/LoginCard/LoginCard.tsx
@@ -3,35 +3,50 @@ import './LoginCard.css';
 import '../../utils/utils.css';
 import { login } from '../../utils/utils';
 import { api } from '../../api/axios';
+import { useNavigate } from 'react-router-dom';
 
 interface props {
     setState: React.Dispatch<React.SetStateAction<any>>
 }
 
-const handleSubmit = (event:any) => {
-    event.preventDefault();
-    console.log('submitted');
-    api.get('/login', {
-        params: {
-        user_email: "dihan.vermeulen12@gmail.com"
-    }})
-    .then(res => console.log(res));
-    // login("dihan.vermeulen12@gmail.com");
-}
+export const LoginCard: React.FC<props> = ({ setState }: props) => {
+    let navigate = useNavigate();
 
-export const LoginCard: React.FC<props> = ({setState}: props) => {
+    const handleSubmit = (event: any) => {
+        event.preventDefault();
+        console.log('submitted');
+        document.querySelector('#loginEmail')
+        // Checks if user exists and password matches then logs user in and creates session
+        api.get('/login', {
+            params: {
+                user_email: "dihan.vermeulen12@gmail.com"
+            }
+        })
+            .then(res => {
+                let resId: any;
+                for (let id of res.data) {
+                    resId = id.user_id;
+                };
+                console.log(resId);
+                login(resId);
+                localStorage.setItem('loggedInAs', JSON.stringify(resId));
+                navigate('/');
+            })
+            .catch((err) => { console.log('error in logging in') });
+    }
+
     return (
         <section className="login-page--card">
             <h1>Sign in</h1>
             <form id='loginForm' method='post' onSubmit={handleSubmit}>
                 <div className='group'>
-                    <input id='email' autoFocus className="input" type="text" required/>
+                    <input id='loginEmail' autoFocus className="input" type="text" required />
                     <span className="highlight"></span>
                     <span className="bar"></span>
                     <label className="label">Email</label>
                 </div>
                 <div className='group'>
-                    <input id='password' className='input' type="text" required/>
+                    <input id='loginPassword' className='input' type="password" required />
                     <span className="highlight"></span>
                     <span className="bar"></span>
                     <label className='label'>Password</label>
@@ -39,7 +54,7 @@ export const LoginCard: React.FC<props> = ({setState}: props) => {
                 <input type='submit' value='Login' />
             </form>
             <p>Need an account?</p>
-            <a className='link' onClick={() => {setState(false)}}>Sign up</a>
+            <a className='link' onClick={() => { setState(false) }}>Sign up</a>
         </section>
     )
 }
