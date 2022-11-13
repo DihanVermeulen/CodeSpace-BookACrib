c1322724d9d3b05567c64460f8420142bc2f15da
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-13 17:03:22 +0200

    Uses localstorage to check if user is signed in

--- bookacrib-client/src/routes/Home.tsx
+++ bookacrib-client/src/routes/Home.tsx
@@ -8,22 +8,37 @@ export const Home = () => {
 
     useEffect(() => {
         navigate('/hotels');
-    }, []);
+        let loggedInAccount: any = localStorage.getItem('loggedInAs');
+        setLoggedInAs(loggedInAccount);
+
+        // Checks to see if user is logged in
+        if (loggedInAccount != null) {
+            console.log('logged in as: ', loggedInAs);
+        }
+        else {
+            navigate('/login&register');
+            console.log('not logged in', loggedInAs);
+        }
+    }, [loggedInAs]);
 
     return (
         <div id="home-page">
             <header className="home-page--main-header">
-                    <img className="home-page--main-header--logo" src={logo}></img>
+                <img className="home-page--main-header--logo" src={logo}></img>
             </header>
             <nav className="home-page--horisontal-navigation">
                 <ul>
                     <li onClick={() => navigate('hotels')}>Hotels</li>
                     <li onClick={() => navigate('Bookings')}>My Bookings</li>
-                    <li onClick={() => navigate('login&register')}>Logout</li>
+                    <li onClick={() => {
+                        setLoggedInAs(null);
+                        localStorage.removeItem('loggedInAs');
+                        navigate('login&register');
+                    }}>Logout</li>
                 </ul>
             </nav>
             <main className="home-page--main-section">
-                <Outlet />
+                <Outlet context={[loggedInAs, setLoggedInAs]} />
             </main>
             <footer className="home-page--footer">
                 All Rights Reserved
