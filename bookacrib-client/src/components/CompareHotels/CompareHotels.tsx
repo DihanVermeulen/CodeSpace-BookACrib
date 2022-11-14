5392fea48a3267598390908682a718f21c0fceb9
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-14 10:39:09 +0200

    Created onsubmit function

--- bookacrib-client/src/components/CompareHotels/CompareHotels.tsx
+++ bookacrib-client/src/components/CompareHotels/CompareHotels.tsx
@@ -29,30 +29,49 @@ export const CompareHotels: React.FC = (): any => {
             });
     }, [hotel])
 
+    const handleSubmit = (event: any) => {
+        event.preventDefault();
+        let arrivalDate = new Date((document.querySelector('#arrivalDate') as HTMLInputElement).value);
+        let departureDate = new Date((document.querySelector('#departureDate') as HTMLInputElement).value);
+
+        let numberOfDays = calculateNumDays(departureDate, arrivalDate);
+        setTotalDays(numberOfDays);
+        if(!isNaN(numberOfDays)) {
+            (document.querySelector('.totalRate') as HTMLElement).style.display = 'block';
+        }
+    }
+
     return (
         <section className="main-section--card">
             <p>Selected hotel: {hotel?.hotel_name}</p>
+            <p>Rates p/d: R{hotel?.hotel_rate}</p>
+            <p className="totalRate">Total Cost: R{totalDays * (hotel?.hotel_rate)}</p>
             <br />
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="main-section--card-form-group">
                     <label>Arrival Date</label>
-                    <input name="arrivalDate" type="date" />
+                    <input id="arrivalDate" name="arrivalDate" type="date" />
                 </div>
                 <div className="main-section--card-form-group">
                     <label>Departure Date</label>
-                    <input name="departureDate" type="date" />
+                    <input id="departureDate" name="departureDate" type="date" />
                 </div>
-                <input type="submit" value="Compare" />
+                <input className="primary-button" type="submit" value="Compare" />
             </form>
+            <button className="primary-button">Make Booking</button>
+
             <hr />
+
             <section className="main-section--card--hotel-section">
                 {allHotels.map((hotel, key) => {
                     return (
                         <article key={key} className='main-section--card--hotel-card'>
                             <img src={hotel.image} alt="hotel"></img>
                             <div>{hotel.hotel_name}</div>
-                            <div>hotel rating:{hotel.hotel_rating}</div>
-                            <button onClick={() => {
+                            <div>Hotel rating:{hotel.hotel_rating}</div>
+                            <div>Rates p/d: R{hotel.hotel_rate}</div>
+                            <p>Total Cost: R{totalDays * hotel.hotel_rate}</p>
+                            <button className="primary-button" onClick={() => {
                                 navigate({
                                     pathname: '/compare-hotels',
                                     search: `?hotel=${key}`,
