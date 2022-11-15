8627f7f85adcbfbe43f524c416d1b7f6361900c3
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-15 09:11:58 +0200

    Added elements

--- bookacrib-client/src/components/Profile/Profile.tsx
+++ bookacrib-client/src/components/Profile/Profile.tsx
@@ -1,5 +1,17 @@
+import update from '../../assets/icons/update.svg';
+import './Profile.css';
+
 export const Profile: React.FC = () => {
     return (
-        <div>Profile</div>
+        <section className="main-section--card">
+            <img className='update-button' src={update} alt="update" />
+            <h2>Username</h2>
+            <h3>Email</h3>
+            <div className='flex-row justify-center'>
+                <div className='badge'>Current bookings: </div>
+                <div className='badge'>Previous bookings: </div>
+                <div className='badge'>Total bookings: </div>
+            </div>
+        </section>
     )
 }
