fb1ad9c2ec8c76d60bfe455dd15d026ed5ababb7
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-16 09:42:15 +0200

    Added function for updating password

--- bookacrib-client/src/components/UpdateProfile/UpdateProfile.tsx
+++ bookacrib-client/src/components/UpdateProfile/UpdateProfile.tsx
@@ -1,7 +1,57 @@
-export const UpdateProfile: React.FC = () => {
-    return (
-        <section className="main-section--card">
+import '../../utils/utils.css';
+import confirm from '../../assets/icons/confirm.svg';
+import { useNavigate } from 'react-router-dom';
+import { api } from '../../api/axios';
 
+export const UpdateProfile: React.FC = () => {
+    const navigate = useNavigate();
+
+    const updateProfile = (newUsername: string, newPassword: string, email: string) => {
+        const data = JSON.stringify(
+            {
+                user_email: email,
+                new_username: newUsername,
+                new_password: newPassword
+            }
+        );
+        api.put('/update-profile', data, {
+            headers: {
+                'Authorization': 'Basic xxxxxxxxxxxxxxxxxxx',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((res) => console.log(res));
+    }
+
+    return (
+        <section id="update-profile" className="main-section--card color-black">
+            <div className='flex-col align-center'>
+                <img className='confirm-button' src={confirm} alt='confirm' onClick={() => {
+                    // navigate('/profile');
+                    updateProfile((document.querySelector('#update-username') as HTMLInputElement).value, 
+                    (document.querySelector('#update-password-confirm') as HTMLInputElement).value,
+                    "dihan.vermeulen12@gmail.com"
+                    );
+                }} />
+                <div className='group color-black text-center justify-center'>
+                    <input id='update-username' autoFocus className="input" type="text" required />
+                    <span className="highlight"></span>
+                    <span className="bar"></span>
+                    <label className="label color-black">Username</label>
+                </div>
+                <div className='group'>
+                    <input id='update-password' className="input" type="password" required />
+                    <span className="highlight"></span>
+                    <span className="bar"></span>
+                    <label className="label color-black">Password</label>
+                </div>
+                <div className='group color-black'>
+                    <input id='update-password-confirm' className="input color-black" type="password" required />
+                    <span className="highlight"></span>
+                    <span className="bar"></span>
+                    <label className="label color-black">Confirm Password</label>
+                </div>
+            </div>
         </section>
     )
 }
