b6f82011c78994374505b15ff21194934189c2ac
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-16 09:41:07 +0200

    icon click navigates to profile update

--- bookacrib-client/src/components/Profile/Profile.tsx
+++ bookacrib-client/src/components/Profile/Profile.tsx
@@ -1,10 +1,14 @@
+import { useNavigate } from 'react-router-dom';
 import update from '../../assets/icons/update.svg';
 import './Profile.css';
 
 export const Profile: React.FC = () => {
+    const navigate = useNavigate();
     return (
-        <section className="main-section--card">
-            <img className='update-button' src={update} alt="update" />
+        <section id='profile' className="main-section--card">
+            <img className='update-button' src={update} alt="update" onClick={() => {
+                navigate('/update-profile');
+            }} />
             <h2>Username</h2>
             <h3>Email</h3>
             <div className='flex-row justify-center'>
