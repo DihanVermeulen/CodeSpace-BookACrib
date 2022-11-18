6f9225057f474c3d16f4e1c1a964f3bf49db5a3b
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-18 16:17:01 +0200

    Updated

--- bookacrib-client/src/components/Profile/Profile.tsx
+++ bookacrib-client/src/components/Profile/Profile.tsx
@@ -3,7 +3,7 @@ import update from '../../assets/icons/update.svg';
 import './Profile.css';
 import { useEffect, useState } from 'react';
 import { api } from '../../api/axios';
-import { getProfile } from '../../utils/utils';
+import { getBookings, getProfile } from '../../utils/functions';
 
 interface User {
     user_name: string,
@@ -12,12 +12,52 @@ interface User {
 
 export const Profile: React.FC = () => {
     const [user, setUser] = useState<User>();
+    const [allBookings, setAllBookings] = useState<any[]>();
+    const [previousBookings, setPreviousBookings] = useState<any[]>();
+    const [upcomingBookings, setUpcomingBookings] = useState<any[]>();
     const navigate = useNavigate();
 
     useEffect(() => {
         getProfile(JSON.parse(localStorage.getItem('loggedInAs') as any)).then((data) => {
             setUser(data[0]);
         });
+
+        getBookings(JSON.parse(localStorage.getItem('loggedInAs') as string))
+            .then((data: any) => {
+                // Sets all bookings
+
+                if (data && Array.isArray(data)) {
+                    setAllBookings(data);
+
+                    // Filters data to find upcoming bookings
+                    let upcomingBookings = data.filter(function (booking: any) {
+                        return new Date(booking.arrival_date) > new Date();
+                    });
+
+                    upcomingBookings = upcomingBookings.map((booking: any) => {
+                        const obj = Object.assign({}, booking);
+                        obj['arrival_date'] = (booking.arrival_date).split(' ')[0];
+                        obj['departure_date'] = (booking.departure_date).split(' ')[0];
+                        return obj;
+                    })
+
+                    setUpcomingBookings(upcomingBookings);
+
+                    // Filters data to find previous bookings
+                    let previousBookings = data.filter(function (booking: any) {
+                        return new Date(booking.arrival_date) < new Date();
+                    });
+
+                    previousBookings = previousBookings.map((booking: any) => {
+                        const obj = Object.assign({}, booking);
+                        obj['arrival_date'] = (booking.arrival_date).split(' ')[0];
+                        obj['departure_date'] = (booking.departure_date).split(' ')[0];
+                        return obj;
+                    })
+
+                    setPreviousBookings(previousBookings);
+                }
+            })
     }, [])
 
     return (
@@ -28,9 +68,9 @@ export const Profile: React.FC = () => {
             <h2>{user?.user_name}</h2>
             <h3>{user?.user_email}</h3>
             <div className='flex-row justify-center'>
-                <div className='badge'>Current bookings: </div>
-                <div className='badge'>Previous bookings: </div>
-                <div className='badge'>Total bookings: </div>
+                <div className='badge'>Upcoming bookings: {upcomingBookings?.length || 0}</div>
+                <div className='badge'>Previous bookings: {previousBookings?.length || 0}</div>
+                <div className='badge'>Total bookings: {allBookings?.length || 0}</div>
             </div>
         </section>
     )
