3ff11a0b418b976ec5f810abaf16a8edecade889
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-18 11:04:11 +0200

    Added more headers

--- bookacrib-server/src/api/index.php
+++ bookacrib-server/src/api/index.php
@@ -13,6 +13,11 @@ $app = new \Slim\App();
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: *');
 header('Access-Control-Allow-Methods: GET,PUT,POST,DELETE,PATCH,OPTIONS');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+header("HTTP/1.1 200 OK");
+
 
 // ======================Hotels==========================
 
