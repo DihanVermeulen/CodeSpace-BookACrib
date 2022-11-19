90ae92c7477a695d2f929f84f709418aba09a91f
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-19 08:21:36 +0200

    Added materialize cdn

--- bookacrib-server/index.php
+++ bookacrib-server/index.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Materialize css cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Materialize javascript cdn -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <title>Book a Crib Administration</title>
 </head>
+
 <body>
-    
+    <?php
+    ?>
 </body>
+
 </html>
