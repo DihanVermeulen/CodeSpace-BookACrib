f932698cc194da1f3b5abe4cfc651981ad6703e1
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-19 15:20:02 +0200

    Created template for CMS

--- bookacrib-server/index.php
+++ bookacrib-server/index.php
@@ -8,14 +8,45 @@
     <!-- Materialize css cdn -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
+    <link rel="stylesheet" href="src\styles\styles.css">
+
     <!-- Materialize javascript cdn -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
     <title>Book a Crib Administration</title>
 </head>
 
 <body>
-    <?php
-    ?>
+    <nav class="nav-extended">
+        <div class="nav-wrapper grey darken-3">
+            <img class="logo" src="src/assets/images/logo.png" alt="logo">
+            <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="sass.html">Login</a></li>
+                <li><a href="sass.html">Logout</a></li>
+            </ul>
+        </div>
+        <div class="nav-content">
+            <ul class="tabs tabs-transparent grey darken-3">
+                <li class="tab"><a class="active" href="#users">Users</a></li>
+                <li class="tab"><a href="#bookings">Bookings</a></li>
+                <li class="tab"><a href="#hotels">Hotels</a></li>
+            </ul>
+        </div>
+    </nav>
+
+
+    <div id="users" class="col s12"><?php
+                                    include_once('src/pages/users.php');
+                                    ?></div>
+    <div id="bookings" class="col s12">Test 2</div>
+    <div id="hotels" class="col s12">Test 4</div>
+    <script src="https://code.jquery.com/jquery-3.6.1.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('.tabs').tabs();
+        });
+    </script>
 </body>
 
 </html>
