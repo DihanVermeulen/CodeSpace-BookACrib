0677a6bb6ff7bdb956ba7fa2d77809ac48172104
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-19 16:25:58 +0200

    Created query to get all users and added to table

---
+++ bookacrib-server/src/pages/users.php
@@ -0,0 +1,36 @@
+<?php
+include_once __DIR__ . './../dbconn/dbconn.php';
+
+$get_all_users_query = "SELECT * FROM users";
+
+try {
+    $result = $db_connection->query($get_all_users_query);
+
+    while ($row = $result->fetch_assoc()) {
+        $usersResponse[] = $row;
+    }
+} catch (PDOException $err) {
+    echo "Error fetching resources: $err";
+}
+?>
+<table class='highlight centered striped'>
+    <thead>
+        <tr>
+            <th>User ID</th>
+            <th>User Name</th>
+            <th>User Email</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <?php
+        foreach ($usersResponse as $row) {
+            echo "<tr>
+                    <td>" . $row['user_id'] . "</td>
+                    <td>" . $row['user_name'] . "</td>
+                    <td>" . $row['user_email'] . "</td>
+                    </tr>";
+        }
+        ?>
+    </tbody>
+</table>
