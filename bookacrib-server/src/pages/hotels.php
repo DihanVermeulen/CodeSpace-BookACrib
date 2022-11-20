9ab080b07ba69f28b287ba134f2ebb8a0d5117a9
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-20 12:27:47 +0200

    Updated

--- bookacrib-server/src/pages/hotels.php
+++ bookacrib-server/src/pages/hotels.php
@@ -83,70 +83,112 @@ try {
     <tbody>
         <?php
         foreach ($hotelsResponse as $row) {
-            echo "<tr>
+            echo "<tr id='" . $row['hotel_id'] . "'>
                     <td>" . $row['hotel_id'] . "</td>
                     <td>" . $row['hotel_name'] . "</td>
                     <td>" . $row['hotel_rating'] . "</td>
                     <td>" . $row['hotel_rate'] . "</td>
                     <td>" . $row['image'] . "</td>
-                    </tr>";
+                </tr>";
         }
         ?>
     </tbody>
 </table>
 
 <div class="container">
-    <div class="row">
-        <form class="col s12" action="" method="POST">
-            <div class="row">
-                <div class="input-field col s12 m4">
-                    <input id="hotel_name_input" name="hotel_name_input" type="text" class="validate" required>
-                    <label for="hotel_name_input">Hotel name</label>
+    <div class="card-panel hoverable">
+
+        <h4>Create new hotel</h4>
+        <div class="row">
+            <form class="col s12" action="" method="POST">
+                <div class="row">
+                    <div class="input-field col s12 m4">
+                        <input id="hotel_name_input" name="hotel_name_input" type="text" class="validate" required>
+                        <label for="hotel_name_input">*Hotel name</label>
+                    </div>
+
+                    <div class="input-field col s12 m4">
+                        <input id="hotel_rating_input" name="hotel_rating_input" type="number" min=1 max=5 class="validate" required>
+                        <label for="hotel_rating_input">*Hotel rating</label>
+                    </div>
+
+
+                    <div class="input-field col s12 m4">
+                        <input id="hotel_rate_input" name="hotel_rate_input" type="number" min=1 class="validate" required>
+                        <label for="hotel_rate_input">*Hotel rate</label>
+                    </div>
                 </div>
 
-                <div class="input-field col s12 m4">
-                    <input id="hotel_rating_input" name="hotel_rating_input" type="number" min=1 max=5 class="validate" required>
-                    <label for="hotel_rating_input">Hotel rating</label>
+                <div class="row">
+                    <div class="input-field center-align">
+                        <button id="hotel_submit" type="submit" name="submit_hotel" class="btn waves-effect waves-light">
+                            Create
+                            <i class="material-icons right">send</i>
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+        <h4>Update hotel</h4>
+        <div class="row">
+            <form class="col s12" action="" method="POST">
+                <div class="row">
+                    <div class="input-field col s12 m4">
+                        <input id="hotel_id_update_input" name="hotel_id_update_input" type="number" class="validate" required>
+                        <label for="hotel_id_update_input">*Hotel ID</label>
+                    </div>
+
+                    <div class="input-field col s12 m4">
+                        <input id="hotel_name_update_input" name="hotel_name_update_input" type="text" class="validate" required>
+                        <label for="hotel_name_update_input">*Hotel name</label>
+                    </div>
+
+                    <div class="input-field col s12 m4">
+                        <input id="hotel_rating_update_input" name="hotel_rating_update_input" type="number" min=1 max=5 class="validate" required>
+                        <label for="hotel_rating_update_input">*Hotel rating</label>
+                    </div>
+
+
+                    <div class="input-field col s12 m4">
+                        <input id="hotel_rate_update_input" name="hotel_rate_update_input" type="number" min=1 class="validate" required>
+                        <label for="hotel_rate_update_input">*Hotel rate</label>
+                    </div>
                 </div>
 
-
-                <div class="input-field col s12 m4">
-                    <input id="hotel_rate_input" name="hotel_rate_input" type="number" min=1 class="validate" required>
-                    <label for="hotel_rate_input">Hotel rate</label>
+                <div class="row">
+                    <div class="input-field center-align">
+                        <button id="hotel_update_submit" type="submit" name="hotel_update_submit" class="btn waves-effect waves-light">
+                            Update
+                            <i class="material-icons right">update</i>
+                        </button>
+                    </div>
                 </div>
-            </div>
-
-            <div class="row">
-                <div class="input-field center-align">
-                    <button id="hotel_submit" type="submit" name="submit_hotel" class="btn waves-effect waves-light">
-                        Submit
-                        <i class="material-icons right">send</i>
-                    </button>
-                </div>
-            </div>
-
-        </form>
+            </form>
+        </div>
     </div>
 </div>
 
 <?php
+// Creates new hotel and inserts into database
 if (isset($_POST['submit_hotel'])) {
     $hotel_name = $_POST['hotel_name_input'];
     $hotel_rating = $_POST['hotel_rating_input'];
     $hotel_rate = $_POST['hotel_rate_input'];
     // Selects random image
-    $hotel_image = random_pic(['la_residence', 'royal_malewena', 'the_silo']);
+    $hotel_image = ['la_residence', 'royal_malewena', 'the_silo'];
 
-    $hotel = new Hotel($hotel_name, $hotel_rating, $hotel_rate, $hotel_image);
-    $hotel_object = $hotel->getHotelObject();
+    $hotel_create = new Hotel($hotel_name, $hotel_rating, $hotel_rate, $hotel_image);
+    $hotel_create_object = $hotel_create->getHotelObject();
 
-    if (!(empty($hotel_object['hotel_name']) && empty($hotel_object['hotel_rating']) && empty($hotel_object['hotel_rate']))) {
+    // Checks if values are empty
+    if (!(empty($hotel_create_object['hotel_name']) && empty($hotel_create_object['hotel_rating']) && empty($hotel_create_object['hotel_rate']))) {
         $insert_hotel_query = "INSERT INTO hotels (hotel_name, hotel_rate, hotel_rating, image) 
         VALUES(?, ?, ?, ?)";
 
         try {
             $stmt = $db_connection->prepare($insert_hotel_query);
-            $stmt->bind_param('siis', $hotel_object['hotel_name'], $hotel_object['hotel_rate'], $hotel_object['hotel_rating'], $hotel_object['hotel_image']);
+            $stmt->bind_param('siis', $hotel_create_object['hotel_name'], $hotel_create_object['hotel_rate'], $hotel_create_object['hotel_rating'], $hotel_create_object['hotel_image']);
 
             $stmt->execute();
         } catch (PDOException $err) {
